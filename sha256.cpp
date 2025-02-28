fcb79206e22a4e24ed523773ae7718dfca5e0293
Mladen Milosevic <jerbdroid@gmail.com>
2025-02-27 23:42:27 -0500

    sha256Update now takes a const Byte

--- sha256.cpp
+++ sha256.cpp
@@ -1,66 +1,74 @@
-/*********************************************************************
-* Filename:   sha256.c
-* Author:     Brad Conte (brad AT bradconte.com)
-* Copyright:
-* Disclaimer: This code is presented "as is" without any guarantees.
-* Details:    Implementation of the SHA-256 hashing algorithm.
-              SHA-256 is one of the three algorithms in the SHA2
-              specification. The others, SHA-384 and SHA-512, are not
-              offered in this implementation.
-              Algorithm specification can be found here:
-               * http://csrc.nist.gov/publications/fips/fips180-2/fips180-2withchangenotice.pdf
-              This implementation uses little endian byte order.
-*********************************************************************/
 
-/*************************** HEADER FILES ***************************/
 #include "sha256.hpp"
+
 #include <memory.h>
-#include <stdlib.h>
+#include <span>
 
 namespace crypto {
 
-/****************************** MACROS ******************************/
-#define ROTLEFT(a, b) (((a) << (b)) | ((a) >> (32 - (b))))
-#define ROTRIGHT(a, b) (((a) >> (b)) | ((a) << (32 - (b))))
+constexpr auto RotateRight(Word a, Word b) -> Word {
+  return (a >> b) | (a << (32 - b));
+}
 
-#define CH(x, y, z) (((x) & (y)) ^ (~(x) & (z)))
-#define MAJ(x, y, z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
-#define EP0(x) (ROTRIGHT(x, 2) ^ ROTRIGHT(x, 13) ^ ROTRIGHT(x, 22))
-#define EP1(x) (ROTRIGHT(x, 6) ^ ROTRIGHT(x, 11) ^ ROTRIGHT(x, 25))
-#define SIG0(x) (ROTRIGHT(x, 7) ^ ROTRIGHT(x, 18) ^ ((x) >> 3))
-#define SIG1(x) (ROTRIGHT(x, 17) ^ ROTRIGHT(x, 19) ^ ((x) >> 10))
+constexpr auto Choose(Word x, Word y, Word z) -> Word {
+  return (x & y) ^ (~x & z);
+}
 
-/**************************** VARIABLES *****************************/
-static const WORD k[64] = {0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-                           0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-                           0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-                           0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-                           0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-                           0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-                           0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-                           0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
+constexpr auto Majority(Word x, Word y, Word z) -> Word {
+  return (x & y) ^ (x & z) ^ (y & z);
+}
 
-/*********************** FUNCTION DEFINITIONS ***********************/
-void sha256_transform(SHA256_CTX* ctx, const BYTE data[]) {
-  WORD a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
+constexpr auto UpperSigma0(Word x) -> Word {
+  return RotateRight(x, 2) ^ RotateRight(x, 13) ^ RotateRight(x, 22);
+}
 
-  for (i = 0, j = 0; i < 16; ++i, j += 4)
+constexpr auto UpperSigma1(Word x) -> Word {
+  return RotateRight(x, 6) ^ RotateRight(x, 11) ^ RotateRight(x, 25);
+}
+
+constexpr auto LowerSigma0(Word x) -> Word {
+  return RotateRight(x, 7) ^ RotateRight(x, 18) ^ (x >> 3);
+}
+
+constexpr auto LowerSigma1(Word x) -> Word {
+  return RotateRight(x, 17) ^ RotateRight(x, 19) ^ (x >> 10);
+}
+
+static std::array<const Word, 64> k = {
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01,
+    0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+    0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da, 0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147,
+    0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070, 0x19a4c116, 0x1e376c08,
+    0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+    0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
+
+void sha256_transform(SHA256_CTX* ctx, std::span<const Byte> data) {
+  Word i;
+  Word j;
+  Word t1;
+  Word t2;
+  std::array<Word, 64> m;
+  for (i = 0, j = 0; i < 16; ++i, j += 4) {
     m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
-  for (; i < 64; ++i)
-    m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
+  }
 
-  a = ctx->state[0];
-  b = ctx->state[1];
-  c = ctx->state[2];
-  d = ctx->state[3];
-  e = ctx->state[4];
-  f = ctx->state[5];
-  g = ctx->state[6];
-  h = ctx->state[7];
+  for (; i < 64; ++i) {
+    m[i] = LowerSigma1(m[i - 2]) + m[i - 7] + LowerSigma0(m[i - 15]) + m[i - 16];
+  }
+
+  Word a = ctx->state[0];
+  Word b = ctx->state[1];
+  Word c = ctx->state[2];
+  Word d = ctx->state[3];
+  Word e = ctx->state[4];
+  Word f = ctx->state[5];
+  Word g = ctx->state[6];
+  Word h = ctx->state[7];
 
   for (i = 0; i < 64; ++i) {
-    t1 = h + EP1(e) + CH(e, f, g) + k[i] + m[i];
-    t2 = EP0(a) + MAJ(a, b, c);
+    t1 = h + UpperSigma1(e) + Choose(e, f, g) + k[i] + m[i];
+    t2 = UpperSigma0(a) + Majority(a, b, c);
     h = g;
     g = f;
     f = e;
@@ -94,7 +102,7 @@ void sha256Init(SHA256_CTX& ctx) {
   ctx.state[7] = 0x5be0cd19;
 }
 
-void sha256Update(SHA256_CTX& ctx, const std::span<BYTE> data) {
+void sha256Update(SHA256_CTX& ctx, const std::span<Byte> data) {
   for (const auto& data_chunk : data) {
     ctx.data[ctx.datalen] = data_chunk;
     ctx.datalen++;
@@ -106,22 +114,24 @@ void sha256Update(SHA256_CTX& ctx, const std::span<BYTE> data) {
   }
 }
 
-void sha256Final(SHA256_CTX& ctx, std::span<BYTE> hash) {
-  WORD i;
+void sha256Final(SHA256_CTX& ctx, std::span<Byte> hash) {
+  Word i;
 
   i = ctx.datalen;
 
   // Pad whatever data is left in the buffer.
   if (ctx.datalen < 56) {
     ctx.data[i++] = 0x80;
-    while (i < 56)
+    while (i < 56) {
       ctx.data[i++] = 0x00;
+    }
   } else {
     ctx.data[i++] = 0x80;
-    while (i < 64)
+    while (i < 64) {
       ctx.data[i++] = 0x00;
+    }
     sha256_transform(&ctx, ctx.data);
-    memset(ctx.data, 0, 56);
+    memset(ctx.data.data(), 0, 56);
   }
 
   // Append to the padding the total message's length in bits and transform.
--- sha256.hpp
+++ sha256.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 /*************************** HEADER FILES ***************************/
+#include <array>
+#include <cstdint>
 #include <span>
 
 namespace crypto {
@@ -9,20 +11,20 @@ namespace crypto {
 constexpr size_t SHA256BlockSize{32};  // SHA256 outputs a 32 byte digestp
 
 /**************************** DATA TYPES ****************************/
-using BYTE = unsigned char;  // 8-bit byte
-using WORD = unsigned int;   // 32-bit word, change to "long" for 16-bit machines
+using Byte = uint8_t;   // 8-bit byte
+using Word = uint32_t;  // 32-bit word, change to "long" for 16-bit machines
 
 struct SHA256_CTX {
-  BYTE data[64];
-  WORD datalen;
+  std::array<Byte, 64> data;
+  Word datalen;
   unsigned long long bitlen;
-  WORD state[8];
+  std::array<Word, 8> state;
 };
 
 /*********************** FUNCTION DECLARATIONS **********************/
 
 void sha256Init(SHA256_CTX& ctx);
-void sha256Update(SHA256_CTX& ctx, const std::span<BYTE> data);  // const BYTE data[], size_t len);
-void sha256Final(SHA256_CTX& ctx, std::span<BYTE> hash);
+void sha256Update(SHA256_CTX& ctx, std::span<const Byte> data);  // const BYTE data[], size_t len);
+void sha256Final(SHA256_CTX& ctx, std::span<Byte> hash);
 
 }  // namespace crypto
